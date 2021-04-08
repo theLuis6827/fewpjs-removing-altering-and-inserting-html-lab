1a35d060b1b2fe66ef2f2dde5c7b5d0b4477b677
theLuis6827 <lozano_22_lalo@yahoo.com>
2021-04-08 18:10:53 -0500

    commit tree afabcef9fde0f448eea10a8d6eb24c8c19787e2a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = '<h1>is the champion</h1>'
+document.body.append(newHeader)
